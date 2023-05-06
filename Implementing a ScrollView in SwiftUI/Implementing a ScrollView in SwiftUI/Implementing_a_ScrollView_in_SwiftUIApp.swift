8ca92907ffcdac86425a187a3e9fbff8acb01993
Elaidzha1940 <eli080293@gmail.com>
2023-05-06 12:29:10 +0700

    Implementing a ScrollView in SwiftUI

---
+++ Implementing a ScrollView in SwiftUI/Implementing a ScrollView in SwiftUI/Implementing_a_ScrollView_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Implementing_a_ScrollView_in_SwiftUIApp.swift
+//  Implementing a ScrollView in SwiftUI
+//
+//  Created by Elaidzha Shchukin on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Implementing_a_ScrollView_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
